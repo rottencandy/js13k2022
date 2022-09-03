c7670bf567659b62c89df93d89ea61e0f343ed38
Mohammed Saud <md.saud020@gmail.com>
2022-09-03 21:39:19 +0530

    finalize cam & panel pos

--- src/entities/panel.ts
+++ src/entities/panel.ts
@@ -1,13 +1,13 @@
 import { createShadedRect } from '../rect';
 import rectFrag from '../shaders/panel.frag';
 
-let obj = createShadedRect(rectFrag, 3, 8);
+let bg = createShadedRect(rectFrag, 3.7, 8.5);
 
 export const update = (dt: number) => {
 };
 
 export const render = () => {
-    bg.use_().draw_(-3,-0.1,.1);
+    bg.use_().draw_(-4,-0.1,.1);
     playBtn.use_().draw_(-2.5, 6.5, .11);
     stopBtn.use_().draw_(-1.5, 6.5, .11);
 };
